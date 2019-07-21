c3fd206e9099f58e567696c5c9e5363eda205008
himself65 <himself65@outlook.com>
2019-07-22 00:28:47 +0800

    style: lint fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,15 @@
 import * as vscode from 'vscode'
+import { displayName as moduleName } from '../package.json'
 
-export function activate(context: vscode.ExtensionContext) {
-
-  console.log('Congratulations, your extension "vscode-hentai" is now active!')
-
+export function activate (context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-
-
     vscode.window.showInformationMessage('Hello World!')
   })
 
   context.subscriptions.push(disposable)
+
+  console.log(`plugin ${moduleName} loaded.`)
 }
 
-export function deactivate() {
+export function deactivate () {
 }
