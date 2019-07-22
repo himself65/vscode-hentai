fa9bb8fc05fc0c5339a8436c8d461730ab74cdf6
himself65 <himself65@outlook.com>
2019-07-23 00:04:40 +0800

    fix: add type of 'event' declare on onDidChangeTextDocument

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,12 @@ import { Observable } from 'rxjs'
 import { throttleTime } from 'rxjs/operators'
 
 export const moduleName = 'vscode-hentai'
-export const observable = new Observable(subscriber => {
+export const observable = new Observable<vscode.TextDocumentChangeEvent>(subscriber => {
   vscode.workspace.onDidChangeTextDocument((event) => subscriber.next(event))
   console.log('workspace event onDidChangeTextDocument registered')
 }).pipe(throttleTime(1000))
 
-observable.subscribe(function onDidChangeTextDocument (value) {
+observable.subscribe(function onDidChangeTextDocument (event) {
   // todo
 })
 
