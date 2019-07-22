9bfa7a01b42df46461593eb2d535aaaf87d2ddac
himself65 <himself65@outlook.com>
2019-07-23 01:04:31 +0800

    feat: use Observer to listen vscode native events

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,24 @@
-import * as vscode from 'vscode'
+import { workspace, TextDocumentChangeEvent, ConfigurationChangeEvent, ExtensionContext } from 'vscode'
 import { Observable } from 'rxjs'
 import { throttleTime } from 'rxjs/operators'
 
 export const moduleName = 'vscode-hentai'
-export const observable = new Observable<vscode.TextDocumentChangeEvent>(subscriber => {
-  vscode.workspace.onDidChangeTextDocument((event) => subscriber.next(event))
-  console.log('workspace event onDidChangeTextDocument registered')
-}).pipe(throttleTime(1000))
 
-observable.subscribe(function onDidChangeTextDocument (event) {
-  // todo
-})
+export let textChangeObserver: Observable<TextDocumentChangeEvent>
+export let configurationObserver: Observable<ConfigurationChangeEvent>
 
-export function activate (context: vscode.ExtensionContext) {
+export function activate (context: ExtensionContext) {
+  textChangeObserver = new Observable<TextDocumentChangeEvent>(subscriber => {
+    workspace.onDidChangeTextDocument((event) => subscriber.next(event))
+  }).pipe(throttleTime(1000))
+  configurationObserver = new Observable<ConfigurationChangeEvent>(subscriber => {
+    workspace.onDidChangeConfiguration(event => subscriber.next(event))
+  })
+
+  textChangeObserver.subscribe(function onDidChangeTextDocument (event) {
+    // todo
+  })
+
   console.log(`plugin ${moduleName} loaded.`)
 }
 
