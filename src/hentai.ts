e1df06f303e65652a313e6b0afa4cc8877aac0a6
himself65 <himself65@outlook.com>
2019-07-24 11:57:15 +0800

    feat: init class Hentai

--- src/hentai.ts
+++ src/hentai.ts
@@ -1,7 +1,23 @@
 import { EventEmitter } from 'events'
-import { On } from './utils'
+import { hentaiKeywords, On } from './utils'
+import { getImages, TInfoItem } from './utils/api'
 
-@On('update', () => {
+@On('update', async (instance: ImageSaver) => instance.update())
+class ImageSaver extends EventEmitter {
+  private lastInfo?: TInfoItem[]
+  private page: number = 0
+  public ImagesUrls?: string[]
+
+  public async update () {
+    const { message, items } = await getImages(hentaiKeywords(), this.page)
+    // fixme: alert to show message
+    if (items != null) {
+      this.lastInfo = items
+    }
+  }
+}
+
+@On('update', (instance: Hentai): void => {
   // todo
 })
 export class Hentai extends EventEmitter {
