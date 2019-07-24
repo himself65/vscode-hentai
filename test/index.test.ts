deaa8f1b4acf9617264a23330601870232055bd5
himself65 <himself65@outlook.com>
2019-07-24 17:22:15 +0800

    test: fix import 'supertest' and simplify import express server

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,9 @@
-// @ts-ignore
-import * as mockServer from '../mock'
-import * as request from 'supertest'
+import request from 'supertest'
 
 describe('Base Test', () => {
   it('server init', done => {
     const port = process.env.PORT
-    const app = mockServer
-    request(app)
+    request('../mock')
       .get('/illusts/keywords=saber')
       .expect(200)
       .expect(/{.+}/, () => done())
